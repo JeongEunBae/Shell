838a5c5e6c50c842d29e68154ee30f093e5fe176
jeongeun <jeongni@ubuntu.(none)>
2020-11-16 02:29:42 -0800

    UPDATE shell

--- CJshell.c
+++ CJshell.c
@@ -1,33 +1,35 @@
-/* jeongeunshell.c */
+/* CJshell.c */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include "shell_signal.h"
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
 
 #define MAX_BUF 256
 
 int getargs(char *cmd, char **argv);
+void handler_SIGINT(int signo, pid_t pid);
+void handler_SIGQUIT(int signo);
 
 int main() 
 {
-	sigInt();
-	sigQuit();
+	signal(SIGINT, handler_SIGINT);
+	signal(SIGTSTP, handler_SIGQUIT);
 	
 	char buf[256];
 	char * argv[50];
 	int narg;
 	pid_t pid;
-	
+	int i = 0;
 	while(1) {
 		printf("CJshell >> ");
 		
 		gets(buf);
-		
+				
 		if(strcmp(buf, "exit") == 0) {
 			printf("finish CJshell :(\n");
 			exit(0);
@@ -63,3 +65,19 @@ int getargs(char *cmd, char ** argv)
 	argv[narg] = NULL;
 	return narg;
 }
+
+void handler_SIGINT(int signo, pid_t pid) {
+	if (kill(pid, SIGTERM) != 0) {
+        	printf("\n");
+    	}
+}
+
+void handler_SIGQUIT(int signo) {
+	printf("\nCtrl+Z(SIGTSTP)\n");
+	printf("Quit (core dumped)\n");
+	
+	exit(1);
+}
+
+
+
